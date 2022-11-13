67f834dcc145d08ebcbc162cfd7e515a93facca4
twrkkk <zhenya.shein.03@gmail.com>
2022-11-13 21:12:21 +0300

    laba 1

---
+++ Permutations with repetition/Permutations with repetition.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+/*
+Написать программу, которая  генерирует слова, содержащие k букв (от «а» до «о»),
+допускающие повторения букв, но никакая буква не должна встречаться более k-2 раз.*/
+
+int K = 0;
+char Letters[] = "абвгдежзиклмно";
+int Count[sizeof Letters] = { 0 };
+
+
+void add_letter(char* s, int n)
+{
+    for (int i = 0; i < sizeof(Letters) - 1; i++) {
+        if (Count[i] <= K - 3)
+        {
+            s[n] = Letters[i];
+            if (n == K - 1) std::cout << s << std::endl;
+            else {
+                ++Count[i];
+                add_letter(s, n + 1);
+                --Count[i];
+            }
+        }
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Введите длину слова " << '\n';
+    std::cin >> K;
+
+    char* s = new char[K + 1]; s[K] = '\0';
+    add_letter(s, 0);
+}
