e1f940a2111efd6a6ec0e864ef6b0591ba842d82
twrkkk <zhenya.shein.03@gmail.com>
2022-12-25 19:01:22 +0300

    greedy algorithm

---
+++ Fill the box/ALG_laba_6.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int length, width, height, k;
+long long cubes[20];
+
+int main() {
+	while (cin >> length>> width>> height >> k) {
+		memset(cubes, 0, sizeof(cubes));
+		for (int i = 0; i < k; i++) {
+			cin >> cubes[i];
+		}
+		long long volume = 1LL * length * width * height;
+		long long result = 0;
+		for (int i = 20; i >= 0; i--) {
+			long long size = 1LL << i;
+			long long count = volume / (size * size * size);
+			if (count > cubes[i]) {
+				count = cubes[i];
+			}
+			result += count;
+			volume -= count * size * size * size;
+			if (volume == 0) {
+				break;
+			}
+		}
+		if (volume == 0) {
+			cout << result << endl;
+		}
+		else {
+			cout << - 1 << endl;
+		}
+	}
+	return 0;
+}
